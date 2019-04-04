21c966e451d78f53689a0cb100dc8188864d5cbf
Yang Jian <narutojian666@gmail.com>
2019-04-04 16:02:05 +0800

    Update hdu P1000.

---
+++ src/HDU/P1000.java
@@ -0,0 +1,16 @@
+package HDU;
+
+import java.util.Scanner;
+
+public class P1000 {
+
+    public static void main(String[] args){
+
+        Scanner cin = new Scanner(System.in);
+
+        while (cin.hasNext())
+            System.out.println(cin.nextInt()+cin.nextInt());
+
+        cin.close();
+    }
+}
