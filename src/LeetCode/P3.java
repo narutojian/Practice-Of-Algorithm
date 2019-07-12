446f2d0fa5fc1de6e4804b0eb41406008a0c7d81
Yang Jian <narutojian666@gmail.com>
2019-07-12 21:15:28 +0800

    Update problem 3 on the platform of Leetcode.

---
+++ src/LeetCode/P3.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+public class P3 {
+
+    public int lengthOfLongestSubstring(String s) {
+
+        if (s.length() == 0)
+            return 0;
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int end = 0;
+        int length = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+
+            for (int j = start; j < end; j++) {
+                if (chars[j] == chars[i]){
+                    start = j+1;
+                    break;
+                }
+            }
+
+            end++;
+            if ((end-start) >length)
+                length = end - start;
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "dvdf";
+        System.out.println(new P3().lengthOfLongestSubstring(s));
+    }
+}
