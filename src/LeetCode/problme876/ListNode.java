0a4a663c608b7de71b855f331672c67486a595f8
narutojian <1213566969@qq.com>
2020-03-23 09:52:32 +0800

    Update P876 in leetcode.

---
+++ src/LeetCode/problme876/ListNode.java
@@ -0,0 +1,7 @@
+package LeetCode.problme876;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ src/LeetCode/problme876/P876.java
@@ -0,0 +1,37 @@
+package LeetCode.problme876;
+
+public class P876 {
+
+    /**
+     * 数组存储节点，返回中间节点
+     * @param head 头指针
+     * @return 中间节点
+     */
+    public ListNode middleNode(ListNode head) {
+        ListNode[] listNodes = new ListNode[100];
+        int index = 0;
+
+        while (head != null) {
+            listNodes[index++] = head;
+            head = head.next;
+        }
+
+        return listNodes[index/2];
+    }
+
+    /**
+     * 快慢指针优化空间，省去数组
+     * @param head 头指针
+     * @return 中间节点
+     */
+    public ListNode middleNode2(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
