682a429b4c2fb1429c88476c056c6adb953cf560
narutojian <1213566969@qq.com>
2020-05-19 11:07:17 +0800

    Update 98. Validate Binary Search Tree in leetcode.

---
+++ src/LeetCode/validate_binary_search_tree/Solution.java
@@ -0,0 +1,36 @@
+package LeetCode.validate_binary_search_tree;
+
+import java.util.Stack;
+
+public class Solution {
+
+    /**
+     * 中序遍历 迭代实现
+     * @param root 根节点
+     * @return 是否为二叉查找树
+     */
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        Integer prev = null;
+        int cur;
+        TreeNode p = root;
+
+        while (!stack.isEmpty() || p != null) {
+            while (p != null) {
+                stack.push(p);
+                p = p.left;
+            }
+            if (!stack.isEmpty()) {
+                p = stack.pop();
+                if (prev == null) prev = p.val;
+                else {
+                    cur = p.val;
+                    if (cur <= prev) return false;
+                    prev = cur;
+                }
+                p = p.right;
+            }
+        }
+        return true;
+    }
+}
