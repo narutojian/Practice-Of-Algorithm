0543ac22244f798218c7c8a1a6d0aa38903850a4
narutojian <1213566969@qq.com>
2021-02-28 10:01:11 +0800

    add 2. Add Two Numbers

---
+++ src/LeetCode/Add_Two_Numbers/Solution.java
@@ -0,0 +1,36 @@
+package LeetCode.Add_Two_Numbers;
+
+/**
+ * 2. Add Two Numbers
+ */
+public class Solution {
+
+    /**
+     * 递归
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode();
+        mergeListNode(res,l1,l2,0);
+        return res.next;
+    }
+
+    private void mergeListNode(ListNode cur,ListNode l1,ListNode l2,int add) {
+        if (l1 == null && l2 == null && add == 0)
+            return;
+        int first = 0;
+        int sec = 0;
+        if (l1 != null) {
+            first = l1.val;
+        }
+        if (l2 != null) {
+            sec = l2.val;
+        }
+        int curVal = first+sec+add;
+        ListNode node = new ListNode(curVal%10);
+        cur.next = node;
+        mergeListNode(cur.next,l1 == null? null : l1.next,l2 == null ? null : l2.next,curVal/10);
+    }
+}
---
+++ src/LeetCode/Add_Two_Numbers/Solution2.java
@@ -0,0 +1,25 @@
+package LeetCode.Add_Two_Numbers;
+
+public class Solution2 {
+
+    /**
+     * 递归
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return dfs(l1,l2,0);
+    }
+
+    private ListNode dfs(ListNode l1,ListNode l2,int add) {
+        if (l1 == null && l2 == null && add == 0)
+            return null;
+        int first = l1 == null ? 0 : l1.val;
+        int sec = l2 == null ? 0 : l2.val;
+        int sum = first+sec+add;
+        ListNode node = new ListNode(sum%10);
+        node.next = dfs(l1 == null ? null : l1.next,l2 == null ? null : l2.next,sum/10);
+        return node;
+    }
+}
