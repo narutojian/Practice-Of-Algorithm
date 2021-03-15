583c12b9954863ec4e4d8e1f8662c60d9d7ed63c
narutojian <1213566969@qq.com>
2021-03-15 11:41:53 +0800

    add 652. Find Duplicate Subtrees

---
+++ src/LeetCode/Find_Duplicate_Subtrees/Solution.java
@@ -0,0 +1,54 @@
+package LeetCode.Find_Duplicate_Subtrees;
+
+import LeetCode.DS.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 652. Find Duplicate Subtrees
+ */
+public class Solution {
+    Map<String,Integer> memo = new HashMap<>();// 记录序列化后的字符串
+    List<TreeNode> res = new ArrayList<>();// 重复的根节点
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        serializeTree(root);
+        return res;
+    }
+
+    /**
+     * 通过序列化每个子树来判断哪些子树是相同的
+     * @param root
+     * @return
+     */
+    private String serializeTree(TreeNode root) {
+        if (root == null)
+            return "#";
+
+        StringBuilder left = new StringBuilder(serializeTree(root.left));
+        StringBuilder right = new StringBuilder(serializeTree(root.right));
+
+        StringBuilder cur = new StringBuilder();
+        cur.append(left);
+        cur.append(',');
+        cur.append(right);
+        cur.append(',');
+        cur.append(root.val);
+
+        String curString = cur.toString();
+
+        if (memo.containsKey(curString)) {
+            // 只出现一次 把重复的添加进结果
+            // 出现多次 则不添加
+            if (memo.get(curString) == 1) {
+                res.add(root);
+                memo.put(curString,2);
+            }
+        }
+        else memo.put(curString,1);
+        return curString;
+    }
+}
